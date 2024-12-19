c501bdb5620c9df0fa66a358429dcec0fc1d04f3
JayFrank\FDC.JayFrank-NC-FE <fdc.jayfrankbarquin@gmail.com>
2024-12-19 17:14:57 +0800

    t

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -4,7 +4,7 @@ import Search from './search'
 const Header = () => {
   return (
     <nav className='flex justify-between items-center p-4 bg-gray-800 text-white px-6'>
-        <Link to='/' className='text-2xl font-bold'>Manga Reader</Link>
+        <Link to='/?page=1' className='text-2xl font-bold'>Manga Reader</Link>
         <Search />
         <ul>
             <li><a href="/">Home</a></li>
---
+++ src/components/home/searchPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const searchPage = props => {
+  return (
+    <div>searchPage</div>
+  )
+}
+
+searchPage.propTypes = {}
+
+export default searchPage
